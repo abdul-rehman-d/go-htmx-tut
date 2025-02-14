fc6d5cfa80f3dda2c374b643ee3dcab0c119aca3
Abdul Rehman <ar618211@gmail.com>
2025-02-14 21:37:19 +0500

    get page, submit new and it's fuckin nice

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"io"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,24 +23,86 @@ func newTemplate() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type Contacts = []Contact
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Data struct {
+	Contacts Contacts
+}
+
+type Store struct {
+	Data Data
+	Form FormData
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newStore() *Store {
+	return &Store{
+		Data: Data{
+			Contacts: Contacts{
+				{"AR", "ar@email.com"},
+				{"AA", "aa@email.com"},
+			},
+		},
+		Form: newFormData(),
+	}
 }
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	count := Count{0}
 	e.Renderer = newTemplate()
 
+	store := newStore()
+
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", store)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if store.Data.hasEmail(email) {
+			store.Form = newFormData()
+			store.Form.Values["name"] = name
+			store.Form.Values["email"] = email
+			store.Form.Errors["email"] = "Email already exists."
+
+			return c.Render(http.StatusUnprocessableEntity, "form", store.Form)
+		}
+
+		contact := Contact{name, email}
+
+		store.Data.Contacts = append(store.Data.Contacts, contact)
+
+		c.Render(200, "form", newFormData())
+		return c.Render(200, "oob-contact", contact)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
